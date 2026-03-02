4fc81145956fcb0612a9768165454fc6da170958
ShaikMohammad8899 <shaikmohammadyaseer@gmail.com>
2026-03-02 13:11:02 +0530

    Add files via upload

---
+++ add1.py
@@ -0,0 +1,10 @@
+Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(20+20)
+40
+>>> print(20*40)
+800
+>>> print(400-100)
+300
+>>> print("python is good")
+python is good
